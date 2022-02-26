bfba1924de9ee51e9ac56a33a1bef5e66b400785
youky7 <1911303982@qq.com>
2022-02-26 23:13:44 +0800

    feat: 完成基本配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  css: {
+    /* CSS 预处理器 */
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "src/style/global.scss";',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '~': __dirname,
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
