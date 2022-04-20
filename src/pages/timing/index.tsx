324f9c5f7d20ec9831924c8484bd3400b5780f5c
Youky1 <youkyf@qq.com>
2022-04-20 20:35:06 +0800

    feat(timing): 完成直接添加计时事项功能

--- src/pages/timing/index.tsx
+++ src/pages/timing/index.tsx
@@ -4,12 +4,21 @@ import {useInitEventList} from '@/hooks/timing';
 import AddButton from '@/component/addButton';
 import Clock from './components/Clock';
 import ItemCard from './components/ItemCard';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {RootState} from '~/@types/store';
+import {Modal, Input} from 'antd';
+import {addEventAction} from '@/redux/timing/actions';
 export default function () {
   const eventList = useInitEventList();
-  const [showAdd, setShowAdd] = useState(false);
   const isTiming = useSelector((state: RootState) => state.timing.isTiming);
+
+  const [showAdd, setShowAdd] = useState(false);
+  const [newEvent, setNewEvent] = useState('');
+  const dispatch = useDispatch();
+  const handleAdd = () => {
+    dispatch(addEventAction(newEvent));
+    setShowAdd(false);
+  };
   return (
     <div className={s.timing}>
       <main>
@@ -19,6 +28,18 @@ export default function () {
       </main>
       <AddButton overlay="添加计时事件" callback={() => setShowAdd(true)} />
       {isTiming && <Clock />}
+      <Modal
+        title="新建计时事件"
+        visible={showAdd}
+        onCancel={() => setShowAdd(false)}
+        onOk={handleAdd}
+      >
+        <Input
+          placeholder="请输入新建事件名"
+          value={newEvent}
+          onChange={e => setNewEvent(e.target.value)}
+        />
+      </Modal>
     </div>
   );
 }
